a703ccc91dd2d9b13fd0b029d5d40d3bdedbfbef
clempaul <clempaul@55864a7c-d784-4d0b-9799-56708a7333a8>
2009-09-12 22:24:27 +0000

    Working version. Needs error handling and code cleanup.

---
+++ trunk/DNS Manager/AddEdit.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace DNS_Manager
+{
+    partial class AddEdit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddEdit));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxRecord = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxType = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxValue = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxComment = new System.Windows.Forms.TextBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Record:";
+            // 
+            // textBoxRecord
+            // 
+            this.textBoxRecord.Location = new System.Drawing.Point(10, 25);
+            this.textBoxRecord.Name = "textBoxRecord";
+            this.textBoxRecord.Size = new System.Drawing.Size(335, 20);
+            this.textBoxRecord.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(319, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "The full name of the record you\'d like to add, e.g. testing.groo.com";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Type:";
+            // 
+            // comboBoxType
+            // 
+            this.comboBoxType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxType.FormattingEnabled = true;
+            this.comboBoxType.Items.AddRange(new object[] {
+            "A",
+            "CNAME",
+            "NS",
+            "PTR",
+            "NAPTR",
+            "SRV",
+            "TXT",
+            "SPF",
+            "AAAA"});
+            this.comboBoxType.Location = new System.Drawing.Point(10, 93);
+            this.comboBoxType.Name = "comboBoxType";
+            this.comboBoxType.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxType.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Value:";
+            // 
+            // textBoxValue
+            // 
+            this.textBoxValue.Location = new System.Drawing.Point(10, 143);
+            this.textBoxValue.Name = "textBoxValue";
+            this.textBoxValue.Size = new System.Drawing.Size(335, 20);
+            this.textBoxValue.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(102, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Comment: (Optional)";
+            // 
+            // textBoxComment
+            // 
+            this.textBoxComment.Location = new System.Drawing.Point(10, 193);
+            this.textBoxComment.Name = "textBoxComment";
+            this.textBoxComment.Size = new System.Drawing.Size(335, 20);
+            this.textBoxComment.TabIndex = 8;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(189, 219);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 9;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(270, 219);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 10;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // AddEdit
+            // 
+            this.AcceptButton = this.buttonSave;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonCancel;
+            this.ClientSize = new System.Drawing.Size(357, 252);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.textBoxComment);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxValue);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBoxType);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxRecord);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AddEdit";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Add Record";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxRecord;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxType;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxValue;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxComment;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonCancel;
+    }
+}
---
+++ trunk/DNS Manager/Listing.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using clempaul;
+using clempaul.Dreamhost.ResponseData;
+
+namespace DNS_Manager
+{
+    public partial class Listing : Form
+    {
+        public Listing()
+        {
+            InitializeComponent();
+        }
+
+        BackgroundWorker GetRecords = new BackgroundWorker();
+        BackgroundWorker DeleteRecord = new BackgroundWorker();
+
+        DreamhostAPI API;
+        IEnumerable<DNSRecord> DNSRecords;
+
+        private void Listing_Load(object sender, EventArgs e)
+        {
+            // Check API Key
+
+            if (Properties.Settings.Default.APIKey == string.Empty &&
+                (new Settings().ShowDialog() == DialogResult.Cancel))
+            {
+                Application.Exit();
+            }
+
+            this.GetRecords.DoWork += new DoWorkEventHandler(GetRecords_DoWork);
+            this.GetRecords.RunWorkerCompleted += new RunWorkerCompletedEventHandler(GetRecords_RunWorkerCompleted);
+
+            this.DeleteRecord.DoWork += new DoWorkEventHandler(DeleteRecord_DoWork);
+            this.DeleteRecord.RunWorkerCompleted += new RunWorkerCompletedEventHandler(DeleteRecord_RunWorkerCompleted);
+
+            this.API = new DreamhostAPI(Properties.Settings.Default.APIKey);
+
+            this.LoadRecords();
+        }
+
+        private void LoadRecords()
+        {
+            if (!this.GetRecords.IsBusy)
+            {
+                this.toolStripStatusLabel.Text = "Loading records...";
+                this.GetRecords.RunWorkerAsync();
+            }
+        }
+
+        void DeleteRecord_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("Record Deleted", "DNS Manager", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.LoadRecords();
+        }
+
+        void DeleteRecord_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (e.Argument.GetType() != typeof(DNSRecord))
+            {
+                throw new Exception("Invalid Argument");
+            }
+
+            this.API.DNS.RemoveRecord((DNSRecord)e.Argument);
+            System.Threading.Thread.Sleep(3000);
+        }
+
+        void GetRecords_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            string zone = this.toolStripComboBoxDomains.Text ?? string.Empty;
+
+            this.toolStripComboBoxDomains.Items.Clear();
+
+            foreach (DNSRecord d in this.DNSRecords)
+            {
+                if (!this.toolStripComboBoxDomains.Items.Contains(d.zone))
+                {
+                    this.toolStripComboBoxDomains.Items.Add(d.zone);
+                }
+            }
+
+            this.toolStripStatusLabel.Text = string.Empty;
+
+            this.toolStripComboBoxDomains.Text = zone;
+            this.SetZone();
+        }
+
+        void GetRecords_DoWork(object sender, DoWorkEventArgs e)
+        {
+            this.DNSRecords = this.API.DNS.ListRecords();
+
+            if (e.Argument != null)
+            {
+                e.Result = true;
+            }
+            else
+            {
+                e.Result = false;
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new About().ShowDialog();
+        }
+
+        private void visitWebsiteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://software.clempaul.me.uk/");
+        }
+
+        private void toolStripButtonSettings_Click(object sender, EventArgs e)
+        {
+            new Settings().ShowDialog();
+        }
+
+        private void toolStripButtonReload_Click(object sender, EventArgs e)
+        {
+            this.LoadRecords();
+        }
+
+        private void toolStripButtonSelectDomain_Click(object sender, EventArgs e)
+        {
+            this.SetZone();
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            if (this.dataGridView.SelectedRows.Count == 1)
+            {
+                DNSRecord record = (DNSRecord)this.dataGridView.SelectedRows[0].DataBoundItem;
+
+                if (!record.editable)
+                {
+                    MessageBox.Show("This record is not editable", "DNS Manager", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (MessageBox.Show("Are you sure you want to delete the record for " + record.record + "?", "DNS Manager", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    this.toolStripStatusLabel.Text = "Deleting record...";
+                    this.DeleteRecord.RunWorkerAsync(record);
+                }
+            }
+        }
+
+        private void toolStripButtonAdd_Click(object sender, EventArgs e)
+        {
+            if (new AddEdit(this.API).ShowDialog() == DialogResult.OK)
+            {
+                this.LoadRecords();
+            }
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            if (this.dataGridView.SelectedRows.Count == 1)
+            {
+                DNSRecord record = (DNSRecord)this.dataGridView.SelectedRows[0].DataBoundItem;
+
+                if (!record.editable)
+                {
+                    MessageBox.Show("This record is not editable", "DNS Manager", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    if (new AddEdit(this.API, record).ShowDialog() == DialogResult.OK)
+                    {
+                        this.LoadRecords();
+                    }
+                }
+            }
+        }
+
+        private void SetZone()
+        {
+            string zoneValue = this.toolStripComboBoxDomains.Text ?? string.Empty;
+
+            this.dataGridView.DataSource = (from d in this.DNSRecords
+                                            where d.zone == zoneValue
+                                            select d).ToList();
+        }
+    }
+}
