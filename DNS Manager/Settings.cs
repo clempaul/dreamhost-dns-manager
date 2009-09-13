34bb7df11c8d3a453129b3343a83426e5f3817b8
Paul Clement <paul@clempaul.me.uk>
2009-09-13 16:31:03 +0000

    Completed Settings.
    Added ability to retry on HTTP errors.

--- DNS Manager/Settings.cs
+++ DNS Manager/Settings.cs
@@ -18,6 +18,35 @@ namespace DNS_Manager
 
         private BackgroundWorker Checker = new BackgroundWorker();
 
+        private void Settings_Load(object sender, EventArgs e)
+        {
+            // Enter existing key
+
+            this.textBoxKey.Text = Properties.Settings.Default.APIKey;
+
+            // Set up Background Worker thread
+
+            this.Checker.DoWork += new DoWorkEventHandler(Checker_DoWork);
+            this.Checker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(Checker_RunWorkerCompleted);
+            this.Checker.WorkerSupportsCancellation = true;
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            // Cancel background worker thread
+
+            if (this.Checker.IsBusy)
+            {
+                this.Checker.CancelAsync();
+            }
+
+            // Close dialog
+
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+
+
         private void buttonSave_Click(object sender, EventArgs e)
         {
             // If key has been entered check it in worker thread, else display error.
@@ -34,44 +63,6 @@ namespace DNS_Manager
             }
         }
 
-        void Checker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
-        {
-            this.buttonSave.Enabled = true;
-            this.labelKeyCheck.Text = string.Empty;
-
-            // If error occurred
-
-            if (e.Result.GetType() == typeof(bool) && (bool)e.Result == false)
-            {
-                MessageBox.Show("An unknown error has occurred", "DNS Manager", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-
-            // If missing functions returned
-
-            else if (e.Result.GetType() == typeof(List<string>) && ((List<string>)e.Result).Count > 0)
-            {
-                string ErrorMessage = "The key does not have access to the following functions:";
-
-                foreach (string m in (List<string>)e.Result)
-                {
-                    ErrorMessage += "\n - " + m;
-                }
-
-                MessageBox.Show(ErrorMessage, "DNS Manager", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-
-            // If key valid
-
-            else
-            {
-                Properties.Settings.Default.APIKey = this.textBoxKey.Text;
-                Properties.Settings.Default.Save();
-
-                this.DialogResult = DialogResult.OK;
-                this.Close();
-            }
-        }
-
         private void Checker_DoWork(object sender, DoWorkEventArgs e)
         {
             // Get API Methods
@@ -79,18 +70,7 @@ namespace DNS_Manager
 
             DreamhostAPI API = new DreamhostAPI(Key);
 
-            IEnumerable<string> Methods;
-
-            try
-            {
-                Methods = API.ListAccessibleMethods();
-            }
-            catch
-            {
-
-                e.Result = false;
-                return;
-            }
+            IEnumerable<string> Methods = API.ListAccessibleMethods();
 
             // Add unavailable methods to list
 
@@ -113,32 +93,58 @@ namespace DNS_Manager
             e.Result = MissingMethods;
         }
 
-        private void buttonCancel_Click(object sender, EventArgs e)
+        void Checker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-            // Cancel background worker thread
+            
 
-            if (this.Checker.IsBusy)
+            // If error occurred
+
+            if (e.Error != null)
             {
-                this.Checker.CancelAsync();
+                // Ask whether to retry.
+
+                if (MessageBox.Show(e.Error.Message, "DNS Manager", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error) == DialogResult.Retry)
+                {
+                    this.Checker.RunWorkerAsync(this.textBoxKey.Text);
+                    return;
+                } 
             }
 
-            // Close dialog
+            // If missing functions returned
 
-            this.DialogResult = DialogResult.Cancel;
-            this.Close();
+            else if (e.Result.GetType() == typeof(List<string>) && ((List<string>)e.Result).Count > 0)
+            {
+                string ErrorMessage = "The key does not have access to the following functions:";
+
+                foreach (string m in (List<string>)e.Result)
+                {
+                    ErrorMessage += "\n - " + m;
+                }
+
+                MessageBox.Show(ErrorMessage, "DNS Manager", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            // If key valid
+
+            else
+            {
+                // Save key
+
+                Properties.Settings.Default.APIKey = this.textBoxKey.Text;
+                Properties.Settings.Default.Save();
+
+                // Set dialog result and return.
+
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            
+            this.buttonSave.Enabled = true;
+            this.labelKeyCheck.Text = string.Empty;
         }
 
-        private void Settings_Load(object sender, EventArgs e)
-        {
-            // Enter existing key
 
-            this.textBoxKey.Text = Properties.Settings.Default.APIKey;
 
-            // Set up Background Worker thread
 
-            this.Checker.DoWork += new DoWorkEventHandler(Checker_DoWork);
-            this.Checker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(Checker_RunWorkerCompleted);
-            this.Checker.WorkerSupportsCancellation = true;
-        }
     }
 }
