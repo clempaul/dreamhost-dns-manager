a7bd100cdb174a11207a46227ee0ce62beed2f87
Paul Clement <paul@clempaul.me.uk>
2009-09-15 14:29:31 +0000

    Error handling for adding records. Extension method for making the first char of a string a capital. Added configuration for x86 to allow debug and continue on x64 machines.

---
+++ DNS Manager/Utilities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DNS_Manager
+{
+    internal static class Utilities
+    {
+        internal static string CapitaliseFirstLetter(this string m)
+        {
+            return m[0].ToString().ToUpper() + m.Remove(0, 1);
+        }
+    }
+}
